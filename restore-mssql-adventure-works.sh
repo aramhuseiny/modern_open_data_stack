c3e059650b82d9ef6d8842837540b7cf61f384bf
Aram Huseiny <huseiny.aram@gmail.com>
2023-12-27 23:27:49 +0000

    fix typo

--- restore-mssql-adventure-works.sh
+++ restore-mssql-adventure-works.sh
@@ -1,6 +1,3 @@
 # #!/bin/bash
-docker exec -it AdventureWorks2022 /opt/mssql-tools/bin/sqlcmd \
-   -S localhost -U SA -P 'Apadanadev@1' \
-   -Q 'RESTORE DATABASE AdventureWorks2022 FROM DISK = "/mnt/AdventureWorks2022.bak" WITH MOVE "AdventureWorks2022" \
-    TO "/var/opt/mssql/data/AdventureWorks2022.mdf", MOVE "AdventureWorks2022_log" \
-    TO "/var/opt/mssql/data/AdventureWorks2022_log.ldf"'
+docker exec -it oltp_initial_msqql_db /opt/mssql-tools/bin/sqlcmd \
+   -S localhost -U SA -P 'Apadanadev@1' -Q 'RESTORE DATABASE AdventureWorks2022 FROM DISK = "/mnt/AdventureWorks2022.bak" WITH MOVE "AdventureWorks2022" TO "/var/opt/mssql/data/AdventureWorks2022.mdf", MOVE "AdventureWorks2022_log"  TO "/var/opt/mssql/data/AdventureWorks2022_log.ldf"'
